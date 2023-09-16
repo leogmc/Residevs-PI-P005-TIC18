8f337544ec53196e05b79afddbff17823dbf8ee6
andre10dias <andre10dias@ymail.com>
2023-09-16 19:57:59 -0300

    Exercícios 4 e 5

--- Exercicio4.cpp
+++ Exercicio4.cpp
@@ -1,42 +1,69 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
+string converteCharParaBinario(unsigned char valor, int &bits);
+
 int main(void) {
-    double x, y, z;
+    unsigned char genePlanta;
+    int verificarGene;
+    int quantidadeBits = 0;
 
-    cout << "Informe o primeiro número de ponto flutuante: ";
-    cin >> x;
+    cout << "Informe o valor do gene da planta (0 a 255): ";
+    cin >> genePlanta;
+    
+    string bits = converteCharParaBinario(genePlanta, quantidadeBits);
+    //cout << "bits: " << bits << endl;
 
-    cout << "Informe o segundo número de ponto flutuante: ";
-    cin >> y;
-
-    cout << endl;
-
-    z = 5 * x + 2;
-
-    if (y > z)
+    cout << "Quantidade de genes presentes: " << quantidadeBits << endl;
+    
+    do
     {
-        cout << "(" << x << ", " << y << "): " << "está à esquerda da curva \n";
-    }
-    else if (y < z)
+        cout << "\nInforme sobre qual gene deseja informações (1 a 8): ";
+        cin >> verificarGene;
+
+        bool valorInvalido = verificarGene < 1 || verificarGene > 8;
+        if (valorInvalido)
+        {
+            cout << "\nO valor deve estar entre 1 e 8.\n\n";
+        }
+    } while (verificarGene < 1 || verificarGene > 8);
+
+    verificarGene--;
+    if (bits.substr(verificarGene, 1) == "1")
     {
-        cout << "(" << x << ", " << y << "): " << "está à direita da curva \n";
+        cout << "\nGene presente na planta.\n";
     }
     else
     {
-        cout << "(" << x << ", " << y << "): " << "está na curva \n";
+        cout << "\nGene ausente na plana.\n";
     }
 
     cout << endl;
-    
-    z = sqrt(x * x + y * y);
-
-    cout << "Distância Euclidiana ao centro das coordenadas (" << x << ", " << y << "): " << z << endl << endl;
-
-    z = x * y;
-    cout << x << " * " << y << " = " << scientific << z << endl << endl;
-
     return 0;
+}
+
+string converteCharParaBinario(unsigned char valor, int &bits) {
+    string s = "";
+    bits = 0;
+
+    for (int i = 7; i >= 0; i--)
+    {
+        //int c = valor & (1 << i);
+        //cout << "valor & (1 << i): " << c << endl; 
+
+        if (valor & (1 << i))
+        {
+            s += "1";
+            bits++;
+            //cout << "S: " << s << endl;
+        }
+        else
+        {
+            s += "0";
+        }
+    }
+
+    return s;
 }
