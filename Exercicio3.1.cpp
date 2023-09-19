65ae67e110bce7aaacaac10fa00a52454222d8d4
andre10dias <andre10dias@ymail.com>
2023-09-19 08:32:27 -0300

    Organizando arquivos

--- Exercicio3.1.cpp
+++ Exercicio3.1.cpp
@@ -6,12 +6,9 @@ using namespace std;
 
 int main(void) {
     int num;
-    short int aux;
 
     cout << "Digite o primeiro numero inteiro: ";
     cin >> num;
-
-    aux = num;
     
     cout << num;
     string resp = num > numeric_limits<short int>::max() ? " é maior que um short int." : num > numeric_limits<short int>::min() && num < numeric_limits<short int>::max() ? " este valor pertence ao intervalo dos short int." : " é menor que um short int.";
